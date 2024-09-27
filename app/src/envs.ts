b0fcff4e909e883c4b28cfabadc7d2ef99c0b524
Agnaldo Carvalho <agjunior.sd@gmail.com>
2024-09-27 06:51:56 +0000

    fix: 🐛 react envs

--- app/src/envs.ts
+++ app/src/envs.ts
@@ -1,4 +1,4 @@
 export const envs = Object.freeze({
-    API_URL: process.env.API_URL || 'http://localhost:3000/',
-    APP_URL: process.env.APP_URL || 'http://localhost',
+    API_URL: process?.env?.REACT_APP_API_URL || 'http://localhost:3000/',
+    APP_URL: process?.env?.REACT_APP_APP_URL || 'http://localhost',
 });
