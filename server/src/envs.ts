28f17d8335fde844b30254d5db58fa87a766ab73
Agnaldo Carvalho <agjunior.sd@gmail.com>
2024-09-27 03:01:54 +0000

    refactor: 💡 remove key and config files

--- server/src/envs.ts
+++ server/src/envs.ts
@@ -1,6 +1,6 @@
 export const envs = Object.freeze({
     API_URL: process.env.API_URL || 'http://localhost:3000',
     APP_URL: process.env.APP_URL || 'http://localhost',
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY || 'AIzaSyAy_pV3huHYonygHX4mBmeOSd9_3CYpgKE',
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY || '',
     DATABSE_URL: process.env.DATABSE_URL || 'file:./dev.db',
 });
